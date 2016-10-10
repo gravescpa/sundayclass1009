739e222f31c14d2d8ff3b05dde706d85b96d21c8
Susan Graves <susan@susangravescpa.com>
2016-10-09 21:29:07 -0400

    cleaned up and ready to deploy

--- app.rb
+++ app.rb
@@ -7,28 +7,23 @@ end
 post '/name' do
 	name = params[:input_name]
 	redirect '/age?input_name=' + name
-
 end
 
 get '/age' do
 	name = params[:input_name].capitalize
 	 erb :get_age, :locals => {:name => name}
-
 end
 
 post '/age' do
 	age = params[:age]
 	name = params[:input_name]
-
 	redirect '/fav_nums?input_name=' + name + '&age=' + age
 end
 
 get '/fav_nums' do
 	age = params[:age]
 	name = params[:input_name]
-
 	erb :fav_nums, :locals => {:name => name, :age => age}
-
 end
 
 post '/fav_nums' do
@@ -37,9 +32,7 @@ post '/fav_nums' do
 	favnum1 = params[:num1]
 	favnum2 = params[:num2]
 	favnum3 = params[:num3]
-
-	 redirect '/change?input_name=' + name + '&age=' + age + '&num1=' + favnum1 + '&num2=' + favnum2 + '&num3=' + favnum3
-	#{}"fav nums are #{favnum1} and #{favnum2} and #{favnum3}"
+	redirect '/change?input_name=' + name + '&age=' + age + '&num1=' + favnum1 + '&num2=' + favnum2 + '&num3=' + favnum3
 end
 
 
@@ -50,11 +43,9 @@ get '/change' do
 	favnum2 = params[:num2].to_i
 	favnum3 = params[:num3].to_i
 	sum = favnum3 + favnum1 + favnum2
-
 	erb :change, :locals => {:name => name, :age => age, :favnum1 => favnum1,
 							 :favnum2 => favnum2, 
 							 :favnum3 => favnum3, :sum => sum }
-
 end
 
 
@@ -63,8 +54,6 @@ post '/change' do
 	input_amount_to_be_reduced = params[:jingle].to_i
 	coins = change(input_amount_to_be_reduced )
 	stuff = hashconverter(coins)
-	
-	# "Thank you #{name}, you have deposited #{stuff}"
 	erb :change_returned, :locals => {:name => name, :stuff => stuff, :input_amount_to_be_reduced => input_amount_to_be_reduced }
 end
 
@@ -76,8 +65,8 @@ get '/contact' do
 	erb :contact
 end
 
-get '/locations' do
-	erb :locations
+get '/location' do
+	erb :location
 end
 
 
--- coin_converter.rb
+++ coin_converter.rb
@@ -1,6 +1,6 @@
 def change(cents_received)
-	coins = {penny: 0, nickle: 0, dime: 0, quarter: 0, half_dollar: 0, silver_dollar: 0}
-	coin_value = {silver_dollar: 100, half_dollar: 50, quarter: 25, dime: 10, nickle: 5, penny: 1}
+	coins = {penny: 0, nickel: 0, dime: 0, quarter: 0, half_dollar: 0, silver_dollar: 0}
+	coin_value = {silver_dollar: 100, half_dollar: 50, quarter: 25, dime: 10, nickel: 5, penny: 1}
 	
 
 	coin_value.each do | coin, value|
@@ -20,7 +20,7 @@ def hashconverter(coins)
 	change = ""
 	coins.each do |key, value|
 		if value > 0
-		change << " " + value.to_s + " " + key.to_s+ "'s"
+		change << " " + value.to_s + " " + key.to_s+ "s"
 	    end
 		
 	end
